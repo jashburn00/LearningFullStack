d146e57947d291900cba43dc70f8ebe9fe196e4e
jashburn00 <jashburn00@gmail.com>
2024-11-21 22:10:53 -0700

    nope it was fun though

--- project_git1/script.js
+++ project_git1/script.js
@@ -1,1 +1,19 @@
-//test
+const words = [
+	['the', 'I\'m', 'Oh', 'Don\'t'] // the 
+	['Russians', 'not', 'my', 'forget'] // russians
+	['are', ] // are
+	[] // nuking 
+	[] // us
+]
+/*
+ * the russians are nuking us
+ * Im not fat im big boned
+ * oh my god they killed kenny
+ * dont forget to bring a towel
+ * */
+
+function generate(){
+	for(let i = 0; i < 6; i++){
+		pickRandom(i);
+	}
+}
