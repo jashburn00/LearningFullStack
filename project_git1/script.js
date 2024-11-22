80c4880c7180af11556d0ad91b2648bf6759f140
jashburn00 <jashburn00@gmail.com>
2024-11-21 22:39:19 -0700

    finish proj_git1 and add to index

--- project_git1/script.js
+++ project_git1/script.js
@@ -1,19 +1,23 @@
 const words = [
-	['the', 'I\'m', 'Oh', 'Don\'t'] // the 
-	['Russians', 'not', 'my', 'forget'] // russians
-	['are', ] // are
-	[] // nuking 
-	[] // us
-]
+    ['The', 'I\'m', 'Oh', 'Don\'t'],
+    ['Russians', 'not', 'my', 'forget'],
+    ['are', 'fat', 'god', 'to'],
+    ['nuking', 'I\'m', 'they', 'bring'],
+    ['us', 'big', 'killed', 'a'],
+    ['', 'boned', 'Kenny', 'towel']
+];
 /*
- * the russians are nuking us
+ * the russians are nuking us ''
  * Im not fat im big boned
  * oh my god they killed kenny
  * dont forget to bring a towel
  * */
 
 function generate(){
+	let msg = '';
 	for(let i = 0; i < 6; i++){
-		pickRandom(i);
+		msg = msg + ' ' + words[i][Math.floor(Math.random()*4)];
 	}
+
+	document.getElementById('msg').innerHTML = msg;
 }
