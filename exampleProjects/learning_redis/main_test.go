3b081698ea9d88b1fd72b19d1aeaad5dbc4bc4f1
jashburn00 <jashburn00@gmail.com>
2026-02-19 08:06:46 -0700

    welp

--- exampleProjects/learning_redis/main_test.go
+++ exampleProjects/learning_redis/main_test.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"os"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
 
 func TestMain(t *testing.T) {
 	//spoofing
@@ -20,49 +19,47 @@ func TestMain(t *testing.T) {
 	defer w.Close()
 	os.Stdout = w
 
-	wg.Go(func() {
-		t.Run("Main should run without panicking", func(t *testing.T) {
-			Main()
-		})
+	t.Run("Main should run without panicking", func(t *testing.T) {
+		//setup
+		t.Parallel()
+		//testing
+		Main()
 	})
-
-	wg.Wait()
 	//unspoofing
 	os.Stdout = originalStdout
 }
 
 func TestCacheIntoRedis(t *testing.T) {
 	//setup
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
 
 	client := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 		DB:   0,
 	})
 
-	wg.Go(func() {
-		t.Run("CacheIntoRedis should cache a key-value pair without error", func(t *testing.T) {
-			err := CacheIntoRedis(ctx, client, "url", "fakeurl.com/abcd", 0)
-			if err != nil {
-				t.Errorf("error encountered: %s", err.Error()) // error: context deadline exceeded
-			}
-		})
+	t.Run("CacheIntoRedis should cache a key-value pair without error", func(t *testing.T) {
+		//setup
+		t.Parallel()
+		//testing
+		err := CacheIntoRedis(context.Background(), client, "url", "fakeurl.com/abcd", 0)
+		//verification
+		if err != nil {
+			t.Errorf("error encountered: %s", err.Error())
+		}
 	})
-
-	wg.Go(func() {
-		t.Run("CacheIntoRedis should obey the given time to live duration", func(t *testing.T) {
-			err := CacheIntoRedis(ctx, client, "Now You See Me", "John Cena", time.Second*1)
-			if err != nil {
-				t.Errorf("error encountered: %s", err.Error())
-			}
-			time.Sleep(time.Second * 2) //timing out a Redis key's TTL removes both the key and value
-			val, err := client.Get(ctx, "Now You See Me").Result()
-			if val != "" {
-				t.Errorf("error encountered: val was %q but should be empty", val)
-			}
-		})
+	t.Run("CacheIntoRedis should obey the given time to live duration", func(t *testing.T) {
+		//setup
+		t.Parallel()
+		//testing
+		err := CacheIntoRedis(context.Background(), client, "Now You See Me", "John Cena", time.Second*1)
+		if err != nil {
+			t.Errorf("error encountered: %s", err.Error())
+		}
+		time.Sleep(time.Second * 2) //timing out a Redis key's TTL removes both the key and value
+		val, err := client.Get(context.Background(), "Now You See Me").Result()
+		//verification
+		if val != "" {
+			t.Errorf("error encountered: val was %q but should be empty", val)
+		}
 	})
-
-	wg.Wait()
 }
